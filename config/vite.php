890861eccdca9fe4fc6cc96658ad82b61c72f6a3
Andi Grether <handplant@users.noreply.github.com>
2025-10-13 11:10:50 +0200

    fix(vite): disable dev server and adjust serverPublic for production build

--- config/vite.php
+++ config/vite.php
@@ -2,11 +2,13 @@
 
 use craft\helpers\App;
 
+$useDevServer = App::env('ENVIRONMENT') === 'dev' || App::env('CRAFT_ENVIRONMENT') === 'dev';
+
 return [
-    'checkDevServer' => true,
+    'checkDevServer' => $useDevServer,
     'devServerInternal' => 'http://localhost:3000',
     'devServerPublic' => App::env('SITE_URL') . ':3000',
-    'manifestPath' => App::env('CRAFT_WEB_ROOT') . '/dist/.vite/manifest.json',
-    'serverPublic' => App::env('SITE_URL') . '/dist/',
-    'useDevServer' => App::env('ENVIRONMENT') === 'dev' || App::env('CRAFT_ENVIRONMENT') === 'dev',
+    'manifestPath' => Craft::getAlias('@webroot') . '/dist/.vite/manifest.json',
+    'serverPublic' => Craft::getAlias('@web') . '/dist/',
+    'useDevServer' => $useDevServer,
 ];

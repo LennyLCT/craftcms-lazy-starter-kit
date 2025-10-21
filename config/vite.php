d569073c5004a2e714683073dc7dc9a463f82ac2
Andi Grether <handplant@users.noreply.github.com>
2025-10-21 12:01:10 +0200

    chore: add nodejs_version to DDEV config and fix vite.php

--- config/vite.php
+++ config/vite.php
@@ -5,7 +5,7 @@ use craft\helpers\App;
 $useDevServer = App::env('ENVIRONMENT') === 'dev' || App::env('CRAFT_ENVIRONMENT') === 'dev';
 
 return [
-    'checkDevServer' => $useDevServer,
+    'checkDevServer' => true,
     'devServerInternal' => 'http://localhost:3000',
     'devServerPublic' => App::env('SITE_URL') . ':3000',
     'manifestPath' => Craft::getAlias('@webroot') . '/dist/.vite/manifest.json',
